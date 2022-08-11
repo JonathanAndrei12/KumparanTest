bc1b9029b81c048687c6101950ef69a9b8eecbf4
JonathanAndrei12 <jonathanandreiwibawa@gmail.com>
2022-08-11 15:02:43 +0700

    Initial Commit

---
+++ Kumparan Recruitment/Kumparan_RecruitmentApp.swift
@@ -0,0 +1,20 @@
+//
+//  Kumparan_RecruitmentApp.swift
+//  Kumparan Recruitment
+//
+//  Created by Jonathan Andrei on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Kumparan_RecruitmentApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
